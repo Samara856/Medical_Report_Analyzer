380b39ee9d7cd67dba83451bbd331ec69fab90b0
Samara <samaratasnim08@gmail.com>
2025-08-26 23:26:06 +0600

    Add Windows/Linux Tesseract path handling

--- ocr_utils.py
+++ ocr_utils.py
@@ -1,7 +1,13 @@
 import pytesseract
 from PIL import Image
+import platform
 
-pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+# OS অনুযায়ী Tesseract path set করা
+if platform.system() == "Windows":
+    pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+else:
+    # Linux server এ সাধারণly /usr/bin/tesseract থাকে
+    pytesseract.pytesseract.tesseract_cmd = "/usr/bin/tesseract"
 
 def extract_text_from_image(file):
     image = Image.open(file)
